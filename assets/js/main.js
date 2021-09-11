f0a6a5dbc66dddc30eae84e95a6e1f413c444f22
jose zambrano <joseyzambranovpe@gmail.com>
2021-09-11 18:07:44 -0500

    cambio de img principal

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,3 +180,38 @@ themeButton.addEventListener('click', () => {
 })
 
 // countdown cuenta regresiva temporizador//
+
+const getRemainTime = deadline =>{
+    let now = new Date(),
+    remainTime = (new Date(deadline) - now + 1000)/1000,
+    remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ('0' + Math.floor(remainTime/60 % 60)).slice(-2),
+    remainHours = ('0' + Math.floor(remainTime/3600 % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
+
+return{
+    remainTime,
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays
+
+}    
+
+};
+
+const countdown = (deadline, elem, finalMessage) =>{
+    const el = document.getElementById(elem)
+
+    const timerUpdate = setInterval (()=>{
+        let t = getRemainTime(deadline);
+        el.innerHTML = `${t.remainDays}d:${t.remainHours}H:${t.remainMinutes}M:${t.remainSeconds}s`
+    if (t.remainTime <=1){
+        clearInterval(timerUpdate);
+        el.innerHTML = finalMessage;
+    }
+},1000)
+    }
+
+    countdown('Sep 11 2021 17:11:46 GMT-0500', 'clock', 'feliz año');
+
